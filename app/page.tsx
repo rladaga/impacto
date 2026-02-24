4ef401a8f07679c54a36ff6b207cbad27ee7a62f
Rodrigo <rladaga@fi.uba.ar>
2026-02-24 20:48:47 -0300

    new map

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ import QuestionsSection from "@/components/QuestionsSection";
 import FooterSection from "@/components/FooterSection";
 import SplashScreen from "@/components/SplashScreen";
 import Image from "next/image";
-import MapStyleEditor from "@/components/MapStyleEditor";
 
 interface Project {
   id: number;
@@ -610,8 +609,6 @@ export default function Home() {
 
       <section className="relative w-full h-screen pt-20">
         <div ref={mapContainer} className="w-full h-full absolute inset-0" />
-
-        {mapLoaded && map.current && <MapStyleEditor map={map.current} />}
 
         {/* SEARCH & FILTER BAR */}
         <div className="absolute top-24 left-4 right-4 md:left-8 md:w-[400px] z-30 flex flex-col gap-2">
